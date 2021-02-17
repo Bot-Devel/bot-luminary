c379ff6db79dbf21a757abbc73435cb74bd29c0a
RogueOne <roguedevone@gmail.com>
2021-02-17 18:14:19 +0530

    Added automatic clearing of infractions and mute using timeout
    - main.py: Added check_user_inf_mute_status() as a background task
    
      - Added check_user_inf_mute_status():
    
        - Checks muted_users and infractions for users and clear them
          after the timeout
        - Added a background task which runs this function every 5 seconds
    
      - Added mod-log message for the mute command
    
    - database.py: Added check_infractions() and check_muted_users()
    
      - Added check_infractions(): Checks the infractions table and gets
                                   user infractions data
      - Added check_muted_users(): Checks the muted_users table and gets
                                   muted_users data
    
    - moderation.py:
    
      - Added get_modlog_mute_msg(): Creates the message for the mute event mod-logs
    
      - Added get_inf_muted_diff(): creates two lists which contains the user_id
                                    and the the time difference between current
                                    time and last triggerd infraction and mute
                                    for the users
    
      - Added get_user_inf_muted_timeout(): Creates two lists which contains the
                                            user_id and the the time difference
                                            between current time and last triggered
                                            infraction and mute for the users

--- utils/database.py
+++ utils/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 
 
 db_file = r"db.sqlite"
@@ -10,11 +11,19 @@ def manage_infractions(message, operation):
     Operation number is needed for the function.
 
     # Returns
-        True {bool} : For operation 2
-        show_infractions {list} : For operation 3, list containing the infractions data for the user
+
+    True {bool} : For operation 2
+
+    show_infractions {list} : For operation 3, list containing the infractions
+                              data for the user
     """
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
+    curr_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+
+    #  to ensure the space between date and time is not causing issues,
+    #  inserting quotes
+    curr_time = '"{}"'.format(curr_time)
 
     if operation == 1:  # insert/update table, add infractions
         cursor.execute(
@@ -23,11 +32,13 @@ def manage_infractions(message, operation):
         if len(cursor.fetchall()) != 0:  # if user found in table
 
             cursor.execute(
-                f"UPDATE infractions SET infractions = infractions + 1, last_triggered = CURRENT_TIME WHERE user_id={message.author.id}"
+                f"UPDATE infractions SET infractions = infractions + 1, last_triggered = {curr_time} WHERE user_id={message.author.id}"
+
             )
         else:
+
             cursor.execute(
-                f"INSERT INTO infractions VALUES ({message.author.id}, 1,CURRENT_TIME)"
+                f"INSERT INTO infractions VALUES ({message.author.id}, 1, {curr_time})"
             )
 
         conn.commit()
@@ -57,14 +68,21 @@ def manage_muted_users(message, operation):
     Operation number is needed for the function.
 
     # Returns
-        True {bool} : For operation 2
-        show_infractions {list} : For operation 3, list containing the infractions data for the user
+
+    True {bool} : For operation 2
+
+    show_muted_users {list} : For operation 3, list containing the infractions
+                              data for the user
     """
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
+    curr_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+
+    # to ensure the space between date and time is not causing issues, inserting quotes
+    curr_time = '"{}"'.format(curr_time)
 
     if operation == 1:  # insert table, add muted_users
-   
+
         if isinstance(message, int):  # if message is the user_id
             cursor.execute(
                 f"SELECT * FROM muted_users WHERE user_id={message}")
@@ -75,11 +93,11 @@ def manage_muted_users(message, operation):
         if len(cursor.fetchall()) == 0:  # if user not found in the table
             if isinstance(message, int):  # if message is the user_id
                 cursor.execute(
-                    f"INSERT INTO muted_users VALUES ({message}, CURRENT_TIME)")
+                    f"INSERT INTO muted_users VALUES ({message}, {curr_time})")
 
             else:
                 cursor.execute(
-                    f"INSERT INTO muted_users VALUES ({message.author.id}, CURRENT_TIME)")
+                    f"INSERT INTO muted_users VALUES ({message.author.id}, {curr_time})")
 
         conn.commit()
         conn.close()
@@ -98,7 +116,53 @@ def manage_muted_users(message, operation):
         cursor.execute(f"SELECT * FROM muted_users \
                              WHERE user_id={message}")
 
-        show_infractions = cursor.fetchall()
+        show_muted_users = cursor.fetchall()
         conn.commit()
         conn.close()
-        return show_infractions
+        return show_muted_users
+
+
+def check_infractions():
+    """
+    Executes insert, update and delete for muted_users table
+    Operation number is needed for the function.
+
+    # Returns
+
+    True {bool} : For operation 2
+
+    show_muted_users {list} : For operation 3, list containing the infractions
+                              data for the user
+    """
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM infractions")
+
+    show_infractions = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    return show_infractions
+
+
+def check_muted_users():
+    """
+    Executes insert, update and delete for muted_users table
+    Operation number is needed for the function.
+
+    # Returns
+
+    True {bool} : For operation 2
+
+    show_muted_users {list} : For operation 3, list containing the infractions
+                              data for the user
+    """
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM muted_users")
+
+    show_muted_users = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    return show_muted_users
